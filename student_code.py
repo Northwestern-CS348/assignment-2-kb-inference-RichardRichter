be7a88b85a6c5588f1ed5206cb95ce91840bad51
Richard Richter <richardrichardrichter2020@u.northwestern.edu>
2019-02-04 15:33:58 -0600

    Updating Student_Code
    Finished the Infer

--- student_code.py
+++ student_code.py
@@ -5,144 +5,196 @@ from logical_classes import *
 verbose = 0
 
 class KnowledgeBase(object):
-    def __init__(self, facts=[], rules=[]):
-        self.facts = facts
-        self.rules = rules
-        self.ie = InferenceEngine()
+   def __init__(self, facts=[], rules=[]):
+       self.facts = facts
+       self.rules = rules
+       self.ie = InferenceEngine()
 
-    def __repr__(self):
-        return 'KnowledgeBase({!r}, {!r})'.format(self.facts, self.rules)
+   def __repr__(self):
+       return 'KnowledgeBase({!r}, {!r})'.format(self.facts, self.rules)
 
-    def __str__(self):
-        string = "Knowledge Base: \n"
-        string += "\n".join((str(fact) for fact in self.facts)) + "\n"
-        string += "\n".join((str(rule) for rule in self.rules))
-        return string
+   def __str__(self):
+       string = "Knowledge Base: \n"
+       string += "\n".join((str(fact) for fact in self.facts)) + "\n"
+       string += "\n".join((str(rule) for rule in self.rules))
+       return string
 
-    def _get_fact(self, fact):
-        """INTERNAL USE ONLY
-        Get the fact in the KB that is the same as the fact argument
+   def _get_fact(self, fact):
+       """INTERNAL USE ONLY
+       Get the fact in the KB that is the same as the fact argument
 
-        Args:
-            fact (Fact): Fact we're searching for
+       Args:
+           fact (Fact): Fact we're searching for
 
-        Returns:
-            Fact: matching fact
-        """
-        for kbfact in self.facts:
-            if fact == kbfact:
-                return kbfact
+       Returns:
+           Fact: matching fact
+       """
+       for kbfact in self.facts:
+           if fact == kbfact:
+               return kbfact
 
-    def _get_rule(self, rule):
-        """INTERNAL USE ONLY
-        Get the rule in the KB that is the same as the rule argument
+   def _get_rule(self, rule):
+       """INTERNAL USE ONLY
+       Get the rule in the KB that is the same as the rule argument
 
-        Args:
-            rule (Rule): Rule we're searching for
+       Args:
+           rule (Rule): Rule we're searching for
 
-        Returns:
-            Rule: matching rule
-        """
-        for kbrule in self.rules:
-            if rule == kbrule:
-                return kbrule
+       Returns:
+           Rule: matching rule
+       """
+       for kbrule in self.rules:
+           if rule == kbrule:
+               return kbrule
 
-    def kb_add(self, fact_rule):
-        """Add a fact or rule to the KB
-        Args:
-            fact_rule (Fact|Rule) - the fact or rule to be added
-        Returns:
-            None
-        """
-        printv("Adding {!r}", 1, verbose, [fact_rule])
-        if isinstance(fact_rule, Fact):
-            if fact_rule not in self.facts:
-                self.facts.append(fact_rule)
-                for rule in self.rules:
-                    self.ie.fc_infer(fact_rule, rule, self)
-            else:
-                if fact_rule.supported_by:
-                    ind = self.facts.index(fact_rule)
-                    for f in fact_rule.supported_by:
-                        self.facts[ind].supported_by.append(f)
-                else:
-                    ind = self.facts.index(fact_rule)
-                    self.facts[ind].asserted = True
-        elif isinstance(fact_rule, Rule):
-            if fact_rule not in self.rules:
-                self.rules.append(fact_rule)
-                for fact in self.facts:
-                    self.ie.fc_infer(fact, fact_rule, self)
-            else:
-                if fact_rule.supported_by:
-                    ind = self.rules.index(fact_rule)
-                    for f in fact_rule.supported_by:
-                        self.rules[ind].supported_by.append(f)
-                else:
-                    ind = self.rules.index(fact_rule)
-                    self.rules[ind].asserted = True
+   def kb_add(self, fact_rule):
+       """Add a fact or rule to the KB
+       Args:
+           fact_rule (Fact|Rule) - the fact or rule to be added
+       Returns:
+           None
+       """
+       printv("Adding {!r}", 1, verbose, [fact_rule])
+       if isinstance(fact_rule, Fact):
+           if fact_rule not in self.facts:
+               self.facts.append(fact_rule)
+               for rule in self.rules:
+                   self.ie.fc_infer(fact_rule, rule, self)
+           else:
+               if fact_rule.supported_by:
+                   ind = self.facts.index(fact_rule)
+                   for f in fact_rule.supported_by:
+                       self.facts[ind].supported_by.append(f)
+               else:
+                   ind = self.facts.index(fact_rule)
+                   self.facts[ind].asserted = True
+       elif isinstance(fact_rule, Rule):
+           if fact_rule not in self.rules:
+               self.rules.append(fact_rule)
+               for fact in self.facts:
+                   self.ie.fc_infer(fact, fact_rule, self)
+           else:
+               if fact_rule.supported_by:
+                   ind = self.rules.index(fact_rule)
+                   for f in fact_rule.supported_by:
+                       self.rules[ind].supported_by.append(f)
+               else:
+                   ind = self.rules.index(fact_rule)
+                   self.rules[ind].asserted = True
 
-    def kb_assert(self, fact_rule):
-        """Assert a fact or rule into the KB
+   def kb_assert(self, fact_rule):
+       """Assert a fact or rule into the KB
 
-        Args:
-            fact_rule (Fact or Rule): Fact or Rule we're asserting
-        """
-        printv("Asserting {!r}", 0, verbose, [fact_rule])
-        self.kb_add(fact_rule)
+       Args:
+           fact_rule (Fact or Rule): Fact or Rule we're asserting
+       """
+       printv("Asserting {!r}", 0, verbose, [fact_rule])
+       self.kb_add(fact_rule)
 
-    def kb_ask(self, fact):
-        """Ask if a fact is in the KB
+   def kb_ask(self, fact):
+       """Ask if a fact is in the KB
 
-        Args:
-            fact (Fact) - Statement to be asked (will be converted into a Fact)
+       Args:
+           fact (Fact) - Statement to be asked (will be converted into a Fact)
 
-        Returns:
-            listof Bindings|False - list of Bindings if result found, False otherwise
-        """
-        print("Asking {!r}".format(fact))
-        if factq(fact):
-            f = Fact(fact.statement)
-            bindings_lst = ListOfBindings()
-            # ask matched facts
-            for fact in self.facts:
-                binding = match(f.statement, fact.statement)
-                if binding:
-                    bindings_lst.add_bindings(binding, [fact])
+       Returns:
+           listof Bindings|False - list of Bindings if result found, False otherwise
+       """
+       print("Asking {!r}".format(fact))
+       if factq(fact):
+           f = Fact(fact.statement)
+           bindings_lst = ListOfBindings()
+           # ask matched facts
+           for fact in self.facts:
+               binding = match(f.statement, fact.statement)
+               if binding:
+                   bindings_lst.add_bindings(binding, [fact])
 
-            return bindings_lst if bindings_lst.list_of_bindings else []
+           return bindings_lst if bindings_lst.list_of_bindings else []
+          
+       else:
+           print("Invalid ask:", fact.statement)
+           return []
 
-        else:
-            print("Invalid ask:", fact.statement)
-            return []
+   def kb_retract(self, fact_or_rule):
+       """Retract a fact from the KB
 
-    def kb_retract(self, fact_or_rule):
-        """Retract a fact from the KB
+       Args:
+           fact (Fact) - Fact to be retracted
 
-        Args:
-            fact (Fact) - Fact to be retracted
+       Returns:
+           None
+       """
+       printv("Retracting {!r}", 0, verbose, [fact_or_rule])
+       ####################################################
+       # Student code goes here
+       if isinstance(fact_or_rule, Fact):
+           for x in self.facts:
+               if x == fact_or_rule:
+                   x.asserted = True
+                   if x.supported_by == []:
+                       self.facts.remove(x)
+                       for y in self.rules:
+                           for z in y.supports_facts:
+                               if z == x:
+                                   y.supports_facts.remove(x)
+       else:
+           for x in self.rules:
+               if x == fact_or_rule:
+                   x.asserted = True
 
-        Returns:
-            None
-        """
-        printv("Retracting {!r}", 0, verbose, [fact_or_rule])
-        ####################################################
-        # Student code goes here
-        
+      
 
 class InferenceEngine(object):
-    def fc_infer(self, fact, rule, kb):
-        """Forward-chaining to infer new facts and rules
+   def fc_infer(self, fact, rule, kb):
+       """Forward-chaining to infer new facts and rules
 
-        Args:
-            fact (Fact) - A fact from the KnowledgeBase
-            rule (Rule) - A rule from the KnowledgeBase
-            kb (KnowledgeBase) - A KnowledgeBase
+       Args:
+           fact (Fact) - A fact from the KnowledgeBase
+           rule (Rule) - A rule from the KnowledgeBase
+           kb (KnowledgeBase) - A KnowledgeBase
 
-        Returns:
-            Nothing            
-        """
-        printv('Attempting to infer from {!r} and {!r} => {!r}', 1, verbose,
-            [fact.statement, rule.lhs, rule.rhs])
-        ####################################################
-        # Student code goes here
+       Returns:
+           Nothing           
+       """
+       printv('Attempting to infer from {!r} and {!r} => {!r}', 1, verbose,
+           [fact.statement, rule.lhs, rule.rhs])
+       ####################################################
+       # Student code goes here
+       #we are going through all the rules comparing to one fact passed through, we are going to see if the new fact triggers any rules
+       see_bindings = match(fact.statement,rule.lhs[0])
+       if see_bindings != match(fact.statement,fact.statement):
+           if see_bindings != False:
+               if len(rule.lhs) > 1:  
+                   list_helper = []
+                   list_helper = rule.lhs
+                   list_lhs = []
+                   for x in rule.lhs:
+                       if x != rule.lhs[0]:
+                           new_statement = instantiate(x, see_bindings)
+                           list_lhs.append(new_statement)
+                   rhs_helper = instantiate(rule.rhs, see_bindings)
+                   list_combinded = []
+                   list_combinded.append(list_lhs)
+                   list_combinded.append(rhs_helper)
+                   inferred_rule = Rule(list_combinded)
+                   pairs = [(rule,fact)]
+                   inferred_rule.supported_by.append(pairs) #may need to look back at this
+                   inferred_rule.asserted = False
+                   fact.supports_rules.append(inferred_rule)
+                   rule.supports_rules.append(inferred_rule)
+                   kb.kb_add(inferred_rule)
+               else: 
+                   new_statement = instantiate(rule.rhs,see_bindings)
+                   new_fact = Fact(new_statement)
+                   pairs = [(rule,fact)]
+                   new_fact.supported_by.append(pairs) #may need to change
+                   new_fact.asserted = False
+                   fact.supports_facts.append(new_fact)
+                   rule.supports_facts.append(new_fact)
+                   kb.kb_add(new_fact)
+              
+#just becasue you retract something, doesnt mean that you remove it, if it has supports, you dont remove it, you do set the assert to false though
+#its alwyas being retracted, doesnt always have to be removed
+#you do not retract rules, you remove a rule if there are no supports, you are only removing the rule if its part of the chain, have a seperate remove function that you call recusrively on the retract function
+
